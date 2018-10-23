42e43e4dc47a1f82f3baf7f19c2ed44ba0c1ad9a
Sophie Virtue <sophie.virtue@gmail.com>
2018-10-23 14:51:46 -0700

    finished media query styling

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
   $('#select-options').on('change', function() {
     const section = $(this).val();
     event.preventDefault();
+    // $('.loading').append(<img src="./build/css/images/ajax-loader.gif">)
 
     let url = 'https://api.nytimes.com/svc/topstories/v2/' + section + '.json';
     url +=
@@ -15,30 +16,29 @@ $(document).ready(function() {
       method: 'GET',
       dataType: 'JSON'
     })
-    .done(function(data) {
-      $('.results').empty();
+      .done(function(data) {
+        $('.results').empty();
+        $('.homescreen-change').addClass();
 
-      let filteredData = data.results.filter(function(info) {
-        return info.multimedia.length;
-      });
-      filteredData = filteredData.slice(0, 12);
-      $.each(filteredData, function(index, value) {
-        $('.results')
-          .append(
-            `<article style="background: url(${
+        let filteredData = data.results.filter(function(info) {
+          return info.multimedia.length;
+        });
+        filteredData = filteredData.slice(0, 12);
+        $.each(filteredData, function(index, value) {
+          $('.results').append(
+            `<a href="${
+              value.url
+            }" target="_blank"><article style="background: url(${
               value.multimedia[4].url
             }) no-repeat center; background-size: cover;"><p class="abstract">${
               value.abstract
-            }</p></article>`
-          )
+            }</p></article></a>`
+          );
+        });
+      })
+      .fail(function() {
+        $('.results').empty();
+        $('.results').append('<p>Apologies, this page is not loading...</p>');
       });
-    });
-          .fail(function() {
-            $('.results').empty();
-            $('.results').append(
-              '<p>Apologies, this page is not loading...</p>'
-            );
-          });
-    
   });
 });
