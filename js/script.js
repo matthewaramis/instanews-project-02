ff17fddb94acf931a8a55952f5cf18d5f4a2ea86
Sophie Virtue <sophie.virtue@gmail.com>
2018-10-23 10:55:09 -0700

    added .js file and started editing .css for article items

--- js/script.js
+++ js/script.js
@@ -1,6 +1,44 @@
 $(document).ready(function() {
-  // js code
+  $('#select-options').on('change', function() {
+    const section = $(this).val();
+    event.preventDefault();
 
-  const myName = 'Sophie';
-  const html = `<h1>Hello ${myName}</h1>`;
+    let url = 'https://api.nytimes.com/svc/topstories/v2/' + section + '.json';
+    url +=
+      '?' +
+      $.param({
+        'api-key': '3a9427a58f294ca2aa06700253bbca28'
+      });
+
+    $.ajax({
+      url: url,
+      method: 'GET',
+      dataType: 'JSON'
+    })
+    .done(function(data) {
+      $('.results').empty();
+
+      let filteredData = data.results.filter(function(info) {
+        return info.multimedia.length;
+      });
+      filteredData = filteredData.slice(0, 12);
+      $.each(filteredData, function(index, value) {
+        $('.results')
+          .append(
+            `<article style="background: url(${
+              value.multimedia[4].url
+            }) no-repeat center; background-size: cover;"><p class="abstract">${
+              value.abstract
+            }</p></article>`
+          )
+      });
+    });
+          .fail(function() {
+            $('.results').empty();
+            $('.results').append(
+              '<p>Apologies, this page is not loading...</p>'
+            );
+          });
+    
+  });
 });
